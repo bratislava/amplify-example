897229851a667465419b592de56c20f02ef2f1b2
Petr Šigut <64728606+shegood@users.noreply.github.com>
2025-08-01 09:34:27 +0200

    Fix login v6 to send Captcha info (#4)
    * remove unneeded package-lock, and add captcha string for the default phone number
    
    * edit the token so that it is obvious that it must be a non-empty string

--- constants.mjs
+++ constants.mjs
@@ -1,6 +1,7 @@
-export const PHONE = "+421948174";
-// export const PHONE = "+42100000000";
+// export const PHONE = "+421948174";
+export const PHONE = "+42100000000";
 export const AWS_REGION = "eu-central-1";
 export const AWS_USER_POOL_ID = "eu-central-1_pXpE6zBM0";
 export const AWS_USER_POOL_WEB_CLIENT_ID = "2f8othmrjillbteieffsknuh1e";
 export const STATIC_PASS = "66febdf7-5b71-4c32-9272-b9d9dd703a60";
+export const TOKEN = "non-empty string";
